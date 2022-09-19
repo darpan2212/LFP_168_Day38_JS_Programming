45bfc8c3585988edaf9d9c5b3b40efde985524c5
Darpan <darpan.madlani@gmail.com>
2022-09-19 21:07:16 +0530

    UC2 - calculate daily wage with switch case

--- empWageBuilder.js
+++ empWageBuilder.js
@@ -1,9 +1,31 @@
 const PRESENT = 1;
+const PART_TIME = 2;
+const FULL_TIME_WORKING_HOUR = 8;
+const WAGE_PER_HOUR = 20;
 
-let isPresent = Math.floor(Math.random() * 2);
+let isPresent = Math.floor(Math.random() * 3);
+let workingHour;
+let dailyWage;
 
-if (isPresent == PRESENT) {
-	console.log("Employee is present");
-} else {
-	console.log("Employee is absent");
+switch(isPresent) {
+
+	case PRESENT :
+		console.log("Employee is present");
+		workingHour = FULL_TIME_WORKING_HOUR;
+		break;
+
+	case PART_TIME :
+		console.log("Employee is working part time");
+		workingHour = FULL_TIME_WORKING_HOUR / 2;
+		break;
+
+	default :
+		console.log("Employee is absent");
+		workingHour = 0;
+		break;
+
 }
+
+dailyWage = workingHour * WAGE_PER_HOUR;
+
+console.log("Daily wage : $"+dailyWage+" USD");
